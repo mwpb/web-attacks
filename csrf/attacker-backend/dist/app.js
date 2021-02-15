dc3f6d418a103e2b32f327a2406d05028390285e
Matthew Burke <matthewpatrickburke@gmail.com>
2021-02-14 20:51:54 -0700

    Demonstrate CORF and SameSite='Strict'

--- csrf/attacker-backend/dist/app.js
+++ csrf/attacker-backend/dist/app.js
@@ -1,8 +1,22 @@
 import express from "express";
 const app = express();
+app.set("view engine", "ejs");
 const port = 8080;
-app.get('/', (req, res) => {
-    res.send('Root of attacker.');
+app.set("view engine", "pug");
+app.get("/", (req, res) => {
+    res.send("Root of attacker.");
+});
+app.get("/startAttack", (req, res) => {
+    let html = "";
+    html += "<html>";
+    html += "<body>";
+    html += "<form method='POST' action='http://defender.localhost/mutateState'>";
+    html += "<input type='hidden' name='key' value='value' />";
+    html += "<input type='submit' value='Submit' />";
+    html += "</form>";
+    html += "</body>";
+    html += "</html>";
+    res.send(html);
 });
 app.listen(port, () => {
     console.log(`Example app listening on http://localhost:${port}`);
--- csrf/attacker-backend/src/app.ts
+++ csrf/attacker-backend/src/app.ts
@@ -1,12 +1,29 @@
 import express from "express";
+import ejs from "ejs";
 
 const app = express();
+app.set("view engine", "ejs");
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('Root of attacker.')
-})
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.send("Root of attacker.");
+});
+
+app.get("/startAttack", (req, res) => {
+  let html = "";
+  html += "<html>";
+  html += "<body>";
+  html += "<form method='POST' action='http://defender.localhost/mutateState'>";
+  html += "<input type='hidden' name='key' value='value' />";
+  html += "<input type='submit' value='Submit' />";
+  html += "</form>";
+  html += "</body>";
+  html += "</html>";
+  res.send(html);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`)
-})
+  console.log(`Example app listening on http://localhost:${port}`);
+});
