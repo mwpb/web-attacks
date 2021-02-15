0ea61a9aff9b0c2a8494ef2d10d1eaf0731dcad6
Matthew Burke <matthewpatrickburke@gmail.com>
2021-02-14 20:00:19 -0700

    Set up docker environment

---
+++ csrf/defender-backend/dist/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+const port = 8080;
+app.get('/', (req, res) => {
+    res.send('Root of defender.');
+});
+app.listen(port, () => {
+    console.log(`Example app listening on http://localhost:${port}`);
+});
